618451250bbcc975c5741c719ff6c506b4dafa97
Oseremhen Ewaleifoh <127787939+MissOseremhen@users.noreply.github.com>
2024-09-28 17:10:14 -0500

    Update tailwind.config.js

--- study-platform/tailwind.config.js
+++ study-platform/tailwind.config.js
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  purge: [
+
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
-};
+}
