77d8a7d3364ab862107bb046772c797d30f6b5dd
ejaifeobuks <97336369+ejaifeobuks@users.noreply.github.com>
2024-09-28 18:13:22 -0500

    Dashboard HTML

--- study-platform/Landing_page/page.js
+++ study-platform/Landing_page/page.js
@@ -2,6 +2,16 @@
   import '../styles/globals.css'
   import 'tailwindcss/tailwind.css'
  
+  // Import Material-UI dependencies
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+// Create your MUI theme (customize if necessary)
+const theme = createTheme({
+  palette: {
+    mode: 'light', // You can change this to 'dark' if needed
+  },
+});
+
    function MyApp({ Component, pageProps }) {
      return <Component {...pageProps} />
    }
---
+++ study-platform/src/app/Dashboard/Page.js
@@ -0,0 +1,13 @@
+export default function Dashboard() {
+    return (
+       <div>
+            <ul>
+                <li><button><h1>Practice</h1></button></li>
+                <li><button><h1>Play</h1></button></li>
+            </ul>
+            <h1>Welcome user</h1>
+            <h1>Subjects</h1>
+            <button></button>
+       </div>  
+    );
+}
