37d7b845a1a88cd22681e68505ef5f692a26ce64
ejaifeobuks <97336369+ejaifeobuks@users.noreply.github.com>
2024-09-28 19:00:20 -0500

    Dashboard
    Created Dashboard

--- study-platform/src/app/Dashboard/page.js
+++ study-platform/src/app/Dashboard/page.js
@@ -1,21 +1,18 @@
-import Image from "next/image";
+// pages/new-page.js
+import Head from 'next/head';
 
-
-export default function Home() {
-  return (
-    <div>
-      <button>Get Started</button>
-    </div>
-  );
-}
-export function NewPage () {
+export default function NewPage() {
   return (
     <div>
       <Head>
         <title>New Page Title</title>
         <meta name="description" content="Description for your new page" />
       </Head>
-      <h1>Welcome to the New Page!</h1>
+      <ul>
+        <li><button>Practice</button></li>
+        <li><button>Play</button></li>
+      </ul>
+      <h1></h1>
       <p>This is the content of your new page.</p>
     </div>
   );
